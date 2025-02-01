6d78f9b1f556d68b68a6e08540fa558d11ead643
Shivamyv <theshivam23679@gmail.com>
2025-02-01 23:28:07 +0530

    Time: 2792 ms (5.01%) | Memory: 422.3 MB (7.32%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.cpp
+++ 115-distinct-subsequences/distinct-subsequences.cpp
@@ -1,8 +1,10 @@
 class Solution {
 public:
     int f(int i,int j,string s, string t,vector<vector<int>>&dp){
+     
      if(j<0) return 1;
-     if(i<0) return 0;
+      if(i<0) return 0;
+    
      
      if(dp[i][j]!=-1) return dp[i][j];
      if(s[i]==t[j]){
