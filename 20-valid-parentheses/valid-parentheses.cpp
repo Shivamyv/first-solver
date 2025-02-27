a3f682edb35fe4d7c04bdee0b0e690cf23c3b83a
Shivamyv <theshivam23679@gmail.com>
2025-02-28 02:14:00 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (62.06%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> stack;
+
+        for (int i = 0; i < s.length(); i++)
+        {
+            if(s[i] == '(' || s[i] == '{' || s[i] == '[')
+            {
+                stack.push(s[i]);
+            }   
+            else if( stack.empty() || abs(s[i] - stack.top()) > 2 )
+            {
+                return false;
+            }
+            else
+            {
+                stack.pop();
+            }
+        }
+        return stack.empty();
+    }
+};
