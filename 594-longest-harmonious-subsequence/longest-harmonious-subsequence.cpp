847113aaac75709fe1db88090861263b8e5a3a0d
Shivamyv <theshivam23679@gmail.com>
2025-06-30 19:33:09 +0530

    Time: 7 ms (94.27%) | Memory: 43.9 MB (29.65%) - LeetSync

---
+++ 594-longest-harmonious-subsequence/longest-harmonious-subsequence.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int findLHS(vector<int>& nums) {
+        unordered_map<int, int> freq;
+        for (int num : nums) {
+            freq[num]++;
+        }
+
+        int maxLen = 0;
+        for (auto& [key, val] : freq) {
+            if (freq.count(key + 1)) {
+                maxLen = max(maxLen, val + freq[key + 1]);
+            }
+        }
+
+        return maxLen;
+    }
+};
