e59ca917f0c729848753e9ba705b4e098f7cbfc1
Shivamyv <theshivam23679@gmail.com>
2025-11-08 01:04:11 +0530

    Time: 7 ms (28.13%) | Memory: 12 MB (9.74%) - LeetSync

---
+++ 383-ransom-note/ransom-note.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine) {
+        
+        unordered_map<char,int>mpp;
+        for(auto it:magazine){
+            mpp[it]++;
+        }
+        for(char ch:ransomNote){
+            if(mpp[ch]==0) return false;
+
+            mpp[ch]--;
+        }
+        return true;
+
+    }
+};
