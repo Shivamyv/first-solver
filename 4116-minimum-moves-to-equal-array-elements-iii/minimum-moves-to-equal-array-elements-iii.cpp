01609c5a7185ef4cf6854eb240e804c76f8f6651
Shivamyv <theshivam23679@gmail.com>
2025-11-09 00:13:13 +0530

    Time: 0 ms (100.00%) | Memory: 31.5 MB (0.00%) - LeetSync

---
+++ 4116-minimum-moves-to-equal-array-elements-iii/minimum-moves-to-equal-array-elements-iii.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int minMoves(vector<int>& nums) {
+        int n=nums.size();
+        int ans=0;
+         int maxi=*max_element(nums.begin(),nums.end());
+         int i=0;
+        for(int i=0;i<n;i++){
+           ans+=(maxi-nums[i]);
+           
+            
+            
+         }
+            
+        return ans;
+
+    }
+};
