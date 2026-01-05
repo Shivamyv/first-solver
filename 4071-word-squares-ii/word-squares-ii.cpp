8a0166966da6f8f0e5f594dc0759ef631624654d
Shivamyv <theshivam23679@gmail.com>
2026-01-05 17:27:48 +0530

    Time: 23 ms (92.31%) | Memory: 60.6 MB (30.77%) - LeetSync

---
+++ 4071-word-squares-ii/word-squares-ii.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    vector<vector<string>> wordSquares(vector<string>& words) {
+     auto s=words;
+     sort(s.begin(),s.end());
+       int n=words.size();
+      
+       vector<vector<string>>ans;
+       
+       for(int a=0;a<n;a++){
+        for(int b=0;b<n;b++){
+            if(a!=b &&s[a][0]==s[b][0]){
+                for(int c=0;c<n;c++){
+                    if(c!=a && c!=b && s[a][3]==s[c][0]){
+                        for(int d=0;d<n;d++){
+                            if(d!=a && d!=b && d!=c && 
+                            s[d][0]==s[b][3]
+                            && s[d][3]==s[c][3]){
+                                ans.push_back({s[a],s[b],s[c],s[d]});
+                            }
+                        }
+                    }
+                }
+            }
+        }
+       }
+
+
+
+     return ans;
+        
+
+
+    }
+};
