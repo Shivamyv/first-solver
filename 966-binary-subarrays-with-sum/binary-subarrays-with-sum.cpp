dc4ccc16b09d62ffb51c09d5edc0b087827b1be8
Shivamyv <theshivam23679@gmail.com>
2026-03-21 01:03:37 +0530

    Time: 0 ms (100.00%) | Memory: 32.7 MB (41.22%) - LeetSync

--- 966-binary-subarrays-with-sum/binary-subarrays-with-sum.cpp
+++ 966-binary-subarrays-with-sum/binary-subarrays-with-sum.cpp
@@ -1,28 +1,29 @@
 class Solution {
 public:
-    int numSubarraysWithSum(vector<int>& nums, int goal) {
-        return numSubarraysWithSumLessEqualToGoal(nums, goal) - numSubarraysWithSumLessEqualToGoal(nums, goal - 1);
-    }
-
- private:  
-    int numSubarraysWithSumLessEqualToGoal(vector<int>& nums, int goal) {
-         int n=nums.size();
-      int sum=0;
+        int f(vector<int>& nums, int goal){
+        int n=nums.size();
+        if(goal < 0) return 0;
+       int i=0;
+       int sum=0;
+      
        int cnt=0;
-     int l=0,r=0;
-     if(goal<0)
-         return 0;
-     while(r<n){
-      sum+=nums[r];
-     
-     while(sum >goal){
-        sum-=nums[l];
-        l++;
-     }
-       cnt=cnt+(r-l+1);
-    r++;
-
+       for(int j=0;j<n;j++){
+        sum+=nums[j];
+        
+        while(sum>goal){
+            sum-=nums[i];
+           
+            i++;
+       }
+        cnt+=j-i+1;
+         
        }
        return cnt;
+
+
     }
+     int numSubarraysWithSum(vector<int>& nums, int goal) {
+      return f(nums,goal)-f(nums,goal-1);  
+    }
+
 };
