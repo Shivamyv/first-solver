4d8f269e81f56455ebede403daa30549c7396e82
Shivamyv <theshivam23679@gmail.com>
2026-03-24 00:55:23 +0530

    Time: 0 ms (100.00%) | Memory: 11.8 MB (15.39%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.cpp
+++ 76-minimum-window-substring/minimum-window-substring.cpp
@@ -1,40 +1,36 @@
 class Solution {
 public:
     string minWindow(string s, string t) {
-      int n=s.size();
-     int sIndex=-1;
-     int minlen=INT_MAX;
-     int l=0,r=0;
-     int cnt=0;
-    int hash[256]={0};
+        int n=s.size();
+      int maxlen=INT_MAX;
+      int index=-1;
+       int hash[256]={0};
+       for(char c:t){
+        hash[c]++;
+       }
+      int l=0,r=0;
+      int count=0;
+      while(r<n){
+          if(hash[s[r]]>0){
+            count++;
+          }
+          hash[s[r]]--;
+         
+         while(count==t.size()){
+         if(r-l+1<maxlen){
+            maxlen=r-l+1;
+            index=l;
+         }
+         hash[s[l]]++;
+         if(hash[s[l]]>0){
+            count--;
+         }
+         l++;
+      }
+      r++;
 
-    for(int i=0;i<t.size();i++){
-        hash[t[i]]++;
-    }
-     while(r<n){
-        if(hash[s[r]]>0){
-            cnt++;
-        }
-        hash[s[r]]--;
-     
-     while(cnt==t.size()){
-        if(r-l+1<minlen){
-        minlen=r-l+1;
-       sIndex=l;
-        }
-        hash[s[l]]++;
-        if(hash[s[l]]>0){
-            cnt--;
-        }
-        l++;
-
-     }
-     r++;
-    }
-     return (sIndex==-1)?"":s.substr(sIndex,minlen);
-
-
-
-
+      }
+      return(index==-1) ? "":s.substr(index,maxlen);
+   
     }
 };
