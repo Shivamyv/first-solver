3abe8e66d769fe0435b016cde5953458dcddf8f4
Shivamyv <theshivam23679@gmail.com>
2026-03-23 23:46:55 +0530

    Time: 2 ms (70.31%) | Memory: 10.8 MB (69.81%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.cpp
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.cpp
@@ -1,56 +1,27 @@
 class Solution {
 public:
     int characterReplacement(string s, int k) {
-        
-int maxLen = 0;
-        
-        
-        int maxFreq = 0;
-        
-        
-        int l = 0, r = 0; 
-        
-      
-        int hash[26] = {0};  
+        int n=s.size();
+       int hash[26]={0};
+       int maxlen=0;
+       int maxfreq=0;
+        int l=0,r=0;
+         while(r<n){
+            hash[s[r]-'A']++;
+            maxfreq=max(maxfreq,hash[s[r]-'A']);
+            if((r-l+1)-maxfreq > k){
+                hash[s[l]-'A']--;
+                l++;
 
-       
-        while (r < s.size()) {
-
-           
-            hash[s[r] - 'A']++;
+            } 
             
-         
-            maxFreq = max(maxFreq, hash[s[r] - 'A']); 
-            
-            
-            while ((r - l + 1) - maxFreq > k) {
-                
-              
-                hash[s[l] - 'A']--;  
-                
-               
-                maxFreq = 0;
-                for (int i = 0; i < 26; ++i) {
-                    maxFreq = max(maxFreq, hash[i]);
-                }
-                
-               
-                l++; 
-                
-            }
-            
-         
-            maxLen = max(maxLen, r - l + 1);
-            
-           
+            maxlen=max(maxlen,r-l+1);
             r++;
-        }
-
+          
+       
+         }
         
-        return maxLen;
-
-
-
+     return maxlen;
 
     }
 };
