89679416c90eef08fb0c1bfe3bf793a1e7a11856
Shivamyv <theshivam23679@gmail.com>
2026-03-26 19:46:26 +0530

    Time: 15 ms (31.45%) | Memory: 11 MB (81.01%) - LeetSync

--- 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.cpp
+++ 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.cpp
@@ -4,16 +4,14 @@ public:
         int n=s.size();
         int l=0,r=0;
         int count=0;
-         vector<int>freq(3,0);
-        while(r<n){
-          freq[s[r]-'a']++;
-          while(freq[0]>0 && freq[1]>0 && freq[2]>0){
-            count+=(n-r);
-          freq[s[l]-'a']--;
-          l++;
-          }
-          r++;          
+       int lastseen[3]={-1,-1,-1};
+       for(int i=0;i<n;i++){
+        lastseen[s[i]-'a']=i;
+        if(lastseen[0]!=-1 && lastseen[1]!=-1 && lastseen[2]!=-1){
+            count+=1+min({lastseen[0],lastseen[1],lastseen[2]});
         }
-    return count;
+
+       }
+       return count;
     }
 };
