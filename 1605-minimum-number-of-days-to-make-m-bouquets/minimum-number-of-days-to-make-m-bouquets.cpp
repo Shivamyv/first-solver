188b42a7cfd6ba7ac9efbdbdd6818466cac8db6f
Shivamyv <theshivam23679@gmail.com>
2026-03-28 01:01:59 +0530

    Time: 14 ms (97.03%) | Memory: 70.6 MB (46.12%) - LeetSync

--- 1605-minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.cpp
+++ 1605-minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.cpp
@@ -1,55 +1,42 @@
 class Solution {
 public:
+ bool possible(vector<int>& bloomDay,int day, int m, int k){
+     int n=bloomDay.size();
+     int cnt=0;
+     int nobucket=0;
+     for(int i=0;i<n;i++){
+       if(bloomDay[i]<=day) cnt++;
+       else{
+        nobucket+=cnt/k;
+        cnt=0;
+       }
+     }
+       nobucket+=cnt/k;
+       return nobucket>=m;
 
-    bool f(vector<int>& bloomDay,int days,int m,int k){
-        int n=bloomDay.size();
-        int cnt=0;
-        int pos=0;
-         for(int i=0;i<n;i++){
-        if(bloomDay[i]<=days){
-      cnt++;
-    }
-    else{
-      pos+=(cnt/k);
-      cnt=0;
-    }
-   }
-  pos+=(cnt/k);
-  return pos >=m;
-    }
-        
-     
-
-
+ }
     int minDays(vector<int>& bloomDay, int m, int k) {
         int n=bloomDay.size();
-        int ans=-1;
-       int maxi=INT_MIN;
-       int mini=INT_MAX;
-       for(int i=0;i<n;i++){
-        maxi=max(maxi,bloomDay[i]);
-        mini=min(mini,bloomDay[i]);
-        }   
-     
-        int low=mini,high=maxi;
-         long long val = m * 1ll * k * 1ll; 
-        if(val > n ) return -1; 
-
+        long long val = m * 1ll * k * 1ll; 
+        if (val > n) return -1; 
+        
+        int maxelement=*max_element(bloomDay.begin(),bloomDay.end());
+        int minelement=*min_element(bloomDay.begin(),bloomDay.end());
+        
+        int low=minelement,high=maxelement ,ans=-1;
         while(low<=high){
-            int mid=(low+high)/2;
-             
-        if(f(bloomDay,mid,m,k)){
-         ans=mid;
-          high=mid-1;
+            int mid=low+(high-low)/2;
+            
+            if(possible(bloomDay,mid,m,k)){
+                ans=mid;
+                high=mid-1;
+            }
+                else{
+                     low=mid+1;
+                }
+            }
+        
+       return ans;
 
-     }
-        else{
-         low=mid+1;
-     }
-     
-    }
-
-
-        return ans;
     }
 };
