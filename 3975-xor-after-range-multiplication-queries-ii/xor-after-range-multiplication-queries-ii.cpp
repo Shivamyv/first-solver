156c031c60b88315600985d774edb8e4a5415a10
Shivamyv <theshivam23679@gmail.com>
2026-04-10 00:14:29 +0530

    Time: 513 ms (38.82%) | Memory: 429.5 MB (34.12%) - LeetSync

--- 3975-xor-after-range-multiplication-queries-ii/xor-after-range-multiplication-queries-ii.cpp
+++ 3975-xor-after-range-multiplication-queries-ii/xor-after-range-multiplication-queries-ii.cpp
@@ -1,96 +1,76 @@
-// class Solution {
-// public:
-//     int xorAfterQueries(vector<int>& nums, 
-//     vector<vector<int>>& queries) {
-//         int n=nums.size();
-//         int q=queries.size();
-//          vector<int>ans;
-//           int mod=1e9+7;
-        
-//        for(int i=0;i<q;i++){
-//        int l= queries[i][0],r=queries[i][1],
-//        k=queries[i][2],v=queries[i][3];
-       
-       
-       
-//         for(int idx=l;idx<=r;idx+=k){
-//             nums[idx]=(1LL*nums[idx]*v)% mod ;
-//         }
-//        }
-        
-//       int cnt=0;
-//         for(int i=0;i<n;i++){
-//            cnt ^= nums[i];
-//        }
-//     return cnt;
-        
-//     }
-// };
-
 class Solution {
 public:
-    
-    long long modPow(long long a, long long b, long long mod) {
-        long long res = 1;
-        a %= mod;
-        while(b > 0) {
-            if(b & 1) res = (res * a) % mod;
-            a = (a * a) % mod;
-            b >>= 1;
-        }
-        return res;
-    }
+int M = 1e9+7;
+    //Binary Exponentiation for Fermat's Little Theorem - > Pow(v ,M-2)
+    long long power(long long a, long long b) {
+        if(b == 0)
+            return 1;
 
-    long long modInverse(long long a, long long mod) {
-        return modPow(a, mod-2, mod);
+        long long half   = power(a, b/2);
+        long long result = (half * half) % M;
+
+        if(b % 2 == 1) {
+            result = (result * a) % M;
+        }
+
+        return result;
     }
 
     int xorAfterQueries(vector<int>& nums, vector<vector<int>>& queries) {
-        long long mod1 = 1e9 + 7;
-        int n = nums.size();
+        int n = nums.size(); 
+        int blockSize = ceil(sqrt(n));
 
-        vector<long long> vis(n+1,1);  
-        vector<long long> vis2(n+2,1); 
+        unordered_map<int, vector<vector<int>>> smallKMap;
 
-        for(auto &it : queries){
-            int u = it[0];
-            int v = it[1];
-            int k = it[2];
-            int vv = it[3];
+        for(auto &query : queries) {
+            int L = query[0];
+            int R = query[1];
+            int K = query[2];
+            int V = query[3];
 
-            if(k == 1){
-                vis[u] = (1LL * vis[u] * vv) % mod1;
-                if(v+1 < n+1) vis[v+1] = (vis[v+1] * modInverse(vv, mod1)) % mod1;
-            }
-            else if(k == 2){
-                vis2[u] = (1LL * vis2[u] * vv) % mod1;
-                if((v-u) % 2 == 0){
-                    if(v+2 < n+2) vis2[v+2] = (vis2[v+2] * modInverse(vv, mod1)) % mod1;
-                } else {
-                    if(v+1 < n+2) vis2[v+1] = (vis2[v+1] * modInverse(vv, mod1)) % mod1;
+            if(K >= blockSize) {
+                for(int i = L; i<= R; i+=K) {
+                    nums[i] = (1LL * nums[i] * V) % M;
                 }
-            }
-            else {
-                for(int i = u; i <= v; i += k){
-                    nums[i] = (1LL * nums[i] * vv) % mod1;
-                }
+            } else { //K < blockSize
+                smallKMap[K].push_back(query);
             }
         }
 
-        for(int i = 2; i < n; i++){
-            vis2[i] = (vis2[i-2] * vis2[i]) % mod1;
-        }
-        for(int i = 1; i < n; i++){
-            vis[i] = (vis[i] * vis[i-1]) % mod1;
-        }
-        for(int i = 0; i < n; i++){
-            vis[i] = (vis[i] * vis2[i]) % mod1;
-        }
-        int ans = 0;
-        for(int i = 0; i < n; i++){
-            ans = (ans ^ (1LL * vis[i] * nums[i]) % mod1);
+        for(auto& [K, allQueries] : smallKMap) {
+            vector<long long> diff(n, 1);
+
+            for(auto& query : allQueries) {
+                int L = query[0];
+                int R = query[1];
+                int V = query[3];
+
+                diff[L] = (diff[L] * V) % M;
+
+                int steps = (R - L)/K;
+                int next  = L + (steps+1)*K;
+
+                if(next < n)
+                    diff[next] = (diff[next] * power(V, M-2)) % M;
+            }
+
+            //Cumulative product
+            for(int i = 0; i < n; i++) {
+                if(i-K >= 0)
+                    diff[i] = (diff[i] * diff[i-K]) % M;
+            }
+
+            //Apply diff to nums
+            for(int i = 0; i < n; i++) {
+                nums[i] = (1LL * nums[i] * diff[i]) % M;
+            }
         }
 
-        return ans;
+        int result = 0;
+        for(int &num : nums) {
+            result = (result ^ num);
+        }
+
+        return result;
     }
 };
