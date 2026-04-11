005427f66a5b7b003c4db75a91b66f3334ad7575
Shivamyv <theshivam23679@gmail.com>
2026-04-11 23:50:15 +0530

    Time: 2 ms (49.52%) | Memory: 37.1 MB (28.12%) - LeetSync

--- 4280-count-digit-appearances/count-digit-appearances.cpp
+++ 4280-count-digit-appearances/count-digit-appearances.cpp
@@ -4,10 +4,7 @@ public:
         int n=nums.size();
         int ans=0;
         for(int i=0;i<n;i++){
-          if(nums[i]==0 && digit==0) {
-            ans++;
-          continue;
-          }
+          
           while(nums[i]>0){
             if(nums[i]%10==digit){
                 ans++;
