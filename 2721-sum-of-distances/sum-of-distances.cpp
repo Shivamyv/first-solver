9d474541992ab6ff3c2f3dfa4a428e0ee7a734a3
Shivamyv <theshivam23679@gmail.com>
2026-04-23 20:05:45 +0530

    Time: 104 ms (27.25%) | Memory: 122.2 MB (36.25%) - LeetSync

--- 2721-sum-of-distances/sum-of-distances.cpp
+++ 2721-sum-of-distances/sum-of-distances.cpp
@@ -1,42 +1,29 @@
 class Solution {
 public:
+typedef long long  ll;
     vector<long long> distance(vector<int>& nums) {
         int n=nums.size();
-        vector<long long>ans(n,0);
-       unordered_map<int,vector<int>>mpp;
+       unordered_map<int,ll>freqcount;
+       unordered_map<int,ll>sumcount;
+       vector<ll>ans(n,0);
        for(int i=0;i<n;i++){
-         mpp[nums[i]].push_back(i);
+          ll freq=freqcount[nums[i]];
+          ll sum=sumcount[nums[i]];
+          ans[i]+=freq*i-sum;
+          freqcount[nums[i]]+=1;
+          sumcount[nums[i]]+=i;
+
        }
-       for(auto it:mpp){
-       vector<int>current=it.second;
-        int idx=current.size();
-        vector<long long>prefix(idx,0);
-        prefix[0]=current[0];
-        for(int i=1;i<idx;i++){
-            prefix[i]=prefix[i-1]+current[i];
-        }
-
-        for(int i=0;i<idx;i++){
-            int index=current[i];
-
-            long long left=0;
-            if(i>0){
-                left=(long long)i*current[i]-prefix[i-1];
-            }
-
-            long long right=0;
-            if(i<idx-1){
-                right=(prefix[idx-1]-prefix[i])-(long long)(idx-i-1)*
-                current[i];
-            }
-            ans[index]=left+right;
-        }
-       
-       }
-       
-      
-   return ans;
-
+       freqcount.clear();
+       sumcount.clear();
+       for(int i=n-1;i>=0;i--){
+          ll freq=freqcount[nums[i]];
+          ll sum=sumcount[nums[i]];
+          ans[i]+=sum-freq*i;
+          freqcount[nums[i]]+=1;
+          sumcount[nums[i]]+=i;
+       }    
+    return ans;
 
 
     }
