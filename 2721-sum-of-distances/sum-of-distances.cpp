c558ab75eff3c142a2b3716cf43a1ec15b38ac48
Shivamyv <theshivam23679@gmail.com>
2026-04-23 19:57:33 +0530

    Time: 99 ms (31.50%) | Memory: 129.7 MB (15.00%) - LeetSync

--- 2721-sum-of-distances/sum-of-distances.cpp
+++ 2721-sum-of-distances/sum-of-distances.cpp
@@ -1,24 +1,43 @@
 class Solution {
 public:
-    vector<long long> distance(vector<int>& nums)
-    {
-        unordered_map<int, vector<int>> um;
-        for(int i=0;i<nums.size();i++){um[nums[i]].push_back(i);}
-        vector<long long> arr(nums.size(), 0);
-        for(auto itr: um)
-        {
-            vector<int> curr = itr.second;
-            if(curr.size()>1)
-            {
-                vector<long long> pref(curr.size()+1);
-                pref[0]=0;
-                for(int i=1;i<curr.size()+1;i++){pref[i]=pref[i-1]+curr[i-1];}
-                for(int i=0;i<curr.size();i++)
-                {
-                    arr[curr[i]]=(2*i+1-curr.size())*(curr[i])+pref[curr.size()]-pref[i+1]-pref[i];
-                }
-            }
+    vector<long long> distance(vector<int>& nums) {
+        int n=nums.size();
+        vector<long long>ans(n,0);
+       unordered_map<int,vector<int>>mpp;
+       for(int i=0;i<n;i++){
+         mpp[nums[i]].push_back(i);
+       }
+       for(auto it:mpp){
+       vector<int>current=it.second;
+        int idx=current.size();
+        vector<long long>prefix(idx,0);
+        prefix[0]=current[0];
+        for(int i=1;i<idx;i++){
+            prefix[i]=prefix[i-1]+current[i];
         }
-        return arr;
+
+        for(int i=0;i<idx;i++){
+            int index=current[i];
+
+            long long left=0;
+            if(i>0){
+                left=(long long)i*current[i]-prefix[i-1];
+            }
+
+            long long right=0;
+            if(i<idx-1){
+                right=(prefix[idx-1]-prefix[i])-(long long)(idx-i-1)*
+                current[i];
+            }
+            ans[index]=left+right;
+        }
+       
+       }
+       
+      
+   return ans;
+
+
+
     }
 };
