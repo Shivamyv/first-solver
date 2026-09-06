8da5a22c813e142c6ec16f9a2962eae2f4c003cb
Shivam Kumar <theshivam23679@gmail.com>
2026-09-06 19:48:11 +0530

    Time: 39 ms (20.36%) | Memory: 27.1 MB (52.57%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.cpp
+++ 115-distinct-subsequences/distinct-subsequences.cpp
@@ -1,35 +1,33 @@
 class Solution {
 public:
 const long long mod=1e9+7;
+   int f(int i,int j,string& s,string& t, vector<vector<int>>&dp){
+    if(j<0) return 1;
+    if(i<0) return 0;
    
+    if(dp[i][j]!=-1) return dp[i][j];
+
+    int result=0;
+    if(s[i]==t[j]){
+      int  move=f(i-1,j-1,s,t,dp);
+       int notmove=f(i-1,j,s,t,dp);
+        result=(move+notmove)%mod;
+    }  
+    else{
+        result=f(i-1,j,s,t,dp)%mod;
+     
+        
+    } 
+    return dp[i][j] =result;
+
+   }
+
     int numDistinct(string s, string t) {
         int n=s.size();
         int m=t.size();
-        vector<vector<int>>dp(n+1,vector<int>(m+1,0));
-       for(int i=0;i<=n;i++){
-        dp[i][0]=1;
-       }
-       for(int j=1;j<=m;j++){
-        dp[0][j]=0;
-       }
-         
-       for(int i=1;i<=n;i++){
-        for(int j=1;j<=m;j++){
-           
-        if(s[i-1]==t[j-1]){
-            int move=dp[i-1][j-1];
-            int notmove=dp[i-1][j];
-            dp[i][j]=(move+notmove)%mod;
-            
-        }
-        else{
-           dp[i][j] = dp[i-1][j] ;
-        }
-         
-        }
-       } 
-
-     return dp[n][m];
+        vector<vector<int>>dp(n+1,vector<int>(m+1,-1));
+      return f(n-1,m-1,s,t,dp);
+       
 
     }
 };
