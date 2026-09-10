9a84fc2b0274643e059794ac2cc7c4d39051306c
Shivam Kumar <theshivam23679@gmail.com>
2026-09-10 12:37:00 +0530

    Time: 7 ms (6.28%) | Memory: 15 MB (27.54%) - LeetSync

--- 130-surrounded-regions/surrounded-regions.cpp
+++ 130-surrounded-regions/surrounded-regions.cpp
@@ -1,55 +1,62 @@
 class Solution {
 public:
-void bfs(int row,int col,vector<vector<char>>&vis,vector<vector<char>>&board,int delrow[],int delcol[]){
- int n=board.size();
- int m=board[0].size();
- queue<pair<int,int>>q;
- q.push({row,col});
- vis[row][col]=1;
- while(!q.empty()){
-    int r=q.front().first;
-    int c=q.front().second;
-    q.pop();
-    for(int i=0;i<4;i++){
-        int nrow=r+delrow[i];
-        int ncol=c+delcol[i];
-        if(nrow>=0 && ncol>=0 && nrow<n && ncol<m && 
-        board[nrow][ncol]=='O' && !vis[nrow][ncol]){
-             vis[nrow][ncol]=1;
-            q.push({nrow,ncol});
-
-        }
-    }
- }
-}
     void solve(vector<vector<char>>& board) {
-        int n=board.size();
-        int m=board[0].size();
-         int delrow[]={-1,0,+1,0};
-         int delcol[]={0,+1,0,-1};
-        vector<vector<char>>vis(n,vector<char>(m,0));
-        
-        for(int j = 0; j < m; j++) {
-            if(!vis[0][j] && board[0][j] == 'O')
-                bfs(0, j, vis, board, delrow, delcol);
-            
-            if(!vis[n-1][j] && board[n-1][j] == 'O')
-                bfs(n-1, j, vis, board, delrow, delcol);
-        }
+     int n=board.size();
+     int m=board[0].size();
+     vector<vector<int>>vis(n,vector<int>(m,0));  
+     queue<pair<int,int>>q; 
+     for(int i=0;i<n;i++){
+        if(board[i][0]=='O'){
+            q.push({i,0});
+            vis[i][0]=1;
 
-        for(int i = 0; i < n; i++) {
-            if(!vis[i][0] && board[i][0] == 'O')
-                bfs(i, 0, vis, board, delrow, delcol);
-
-            if(!vis[i][m-1] && board[i][m-1] == 'O')
-                bfs(i, m-1, vis, board, delrow, delcol);
         }
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(!vis[i][j] && board[i][j]=='O'){
-                    board[i][j]='X';
-                }
+        if(board[i][m-1]=='O'){
+            q.push({i,m-1});
+            vis[i][m-1]=1;
+        }
+     }
+     for(int j=0;j<m;j++){
+        if(board[0][j]=='O'){
+            q.push({0,j});
+            vis[0][j]=1;
+        }
+        if(board[n-1][j]=='O'){
+        q.push({n-1,j});
+        vis[n-1][j]=1;
+        }
+     }
+     int dr[]={-1,0,+1,0};
+     int dc[]={0,+1,0,-1};
+     while(!q.empty()){
+        auto it=q.front();
+        q.pop();
+        int row=it.first;
+        int col=it.second;
+       
+       for(int k=0;k<4;k++){
+        int nrow=dr[k]+row;
+        int ncol=dc[k]+col;
+        if(nrow>=0 && ncol>=0 && nrow<n && ncol<m && !vis[nrow][ncol] && board[nrow][ncol]=='O'){
+          
+            q.push({nrow,ncol});
+            vis[nrow][ncol]=1;
+        }
+       }
+          
+     }
+     for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            if(board[i][j]=='O' && !vis[i][j]){
+                board[i][j]='X';
+                vis[i][j]=1;
+
             }
         }
+     }
+   
+
+
+
     }
 };
