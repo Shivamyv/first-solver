edb557625277edb8bbb537fc21bd501b933044a6
Shivam Kumar <theshivam23679@gmail.com>
2026-09-10 12:52:13 +0530

    Time: 4 ms (89.68%) | Memory: 31.6 MB (82.35%) - LeetSync

--- 1073-number-of-enclaves/number-of-enclaves.cpp
+++ 1073-number-of-enclaves/number-of-enclaves.cpp
@@ -1,82 +1,56 @@
 class Solution {
 public:
-void bfs(int i,int j,vector<vector<int>>&vis,vector<vector<int>>& grid){
-    int m=grid.size();
-    int n=grid[0].size();
-    
-  
+    int numEnclaves(vector<vector<int>>& grid) {
+    int n=grid.size();
+    int m=grid[0].size();
     queue<pair<int,int>>q;
-    q.push({i,j});
-    vis[i][j]=1;
-    int dr[]={-1,0,+1,0};
-    int dc[]={0,+1,0,-1};
-    while(!q.empty()){
+    vector<vector<int>>vis(n,vector<int>(m,0));
+   for(int i=0;i<n;i++){
+        if(grid[i][0]==1){
+            q.push({i,0});
+            vis[i][0]=1;
+
+        }
+        if(grid[i][m-1]==1){
+            q.push({i,m-1});
+            vis[i][m-1]=1;
+        }
+     }
+     for(int j=0;j<m;j++){
+        if(grid[0][j]==1){
+            q.push({0,j});
+            vis[0][j]=1;
+        }
+        if(grid[n-1][j]==1){
+        q.push({n-1,j});
+        vis[n-1][j]=1;
+        }
+     }
+      int dr[]={-1,0,+1,0};
+     int dc[]={0,+1,0,-1};
+     while(!q.empty()){
         auto it=q.front();
         q.pop();
         int row=it.first;
         int col=it.second;
-
         for(int i=0;i<4;i++){
-            int nrow=row+dr[i];
-            int ncol=col+dc[i];
-            if(nrow>=0 && ncol>=0 && nrow<m && ncol<n && !vis[nrow][ncol]&& 
+            int nrow=dr[i]+row;
+            int ncol=dc[i]+col;
+
+            if(nrow>=0 && ncol>=0 && nrow<n && ncol<m && !vis[nrow][ncol] && 
             grid[nrow][ncol]==1){
-                vis[nrow][ncol]=1;
                 q.push({nrow,ncol});
-
+                vis[nrow][ncol]=1;
             }
         }
-
-
-
-    }
-    
-
-
-}
-    int numEnclaves(vector<vector<int>>& grid) {
-        int m=grid.size();
-        int n=grid[0].size();
-        
-        vector<vector<int>>vis(m,vector<int>(n,0));
-         for(int i = 0; i < m; i++) {
-
-            if(grid[i][0] == 1 && !vis[i][0]) {
-                bfs(i, 0, vis, grid);
-            }
-
-            if(grid[i][n-1] == 1 && !vis[i][n-1]) {
-                bfs(i, n-1, vis, grid);
-            }
+     }
+     int ans=0;
+     for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            if(grid[i][j]==1 && !vis[i][j]) ans++;
         }
-
-        // first and last row
-        for(int j = 0; j < n; j++) {
-
-            if(grid[0][j] == 1 && !vis[0][j]) {
-                bfs(0, j, vis, grid);
-            }
-
-            if(grid[m-1][j] == 1 && !vis[m-1][j]) {
-                bfs(m-1, j, vis, grid);
-            }
-        }
-
-        int cnt = 0;
-
-        for(int i = 0; i < m; i++) {
-            for(int j = 0; j < n; j++) {
-
-                if(grid[i][j] == 1 && !vis[i][j]) {
-                    cnt++;
-                }
-            }
-        }
-
-        return cnt;
-
-
-
+     }
+      return ans;    
 
     }
 };
